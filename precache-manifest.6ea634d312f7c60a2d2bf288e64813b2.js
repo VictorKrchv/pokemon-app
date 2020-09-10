8f8fe0f7a0ad0fcf1e5ecdf7240385d519b9b06e
VictorKrchv <bublikoo228@gmail.com>
2020-09-10 23:37:21 +0300

    Updates

---
+++ precache-manifest.6ea634d312f7c60a2d2bf288e64813b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b136a8413cc7ff948c4df6c45e2371c",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "64f8de2386fb89b777aa",
+    "url": "/pokemon-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e79cbf8fe00c80a63ef",
+    "url": "/pokemon-app/static/js/2.a2905da3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-app/static/js/2.a2905da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f8de2386fb89b777aa",
+    "url": "/pokemon-app/static/js/main.3cb88860.chunk.js"
+  },
+  {
+    "revision": "54fdf9bb8c7b6f5bdf9d",
+    "url": "/pokemon-app/static/js/runtime-main.0c6dddaf.js"
+  },
+  {
+    "revision": "ef533b6454335e69014da48b11d159bc",
+    "url": "/pokemon-app/static/media/preloader.ef533b64.svg"
+  }
+]);
